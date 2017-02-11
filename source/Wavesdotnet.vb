7d4c8774a198a56026c21793dc39add98a2e56de
Sphearis <omedian@gmail.com>
2017-02-11 12:33:00 +0100

    fix assetID class for testing of getassetlist

--- source/Wavesdotnet.vb
+++ source/Wavesdotnet.vb
@@ -1,6 +1,7 @@
-﻿Imports System.Net
+Imports System.Net
 
 Public Class WalletConnection
+    
     Public Property address As String
     Public Property confirmations As Integer
     Public Property balance As Double
@@ -10,11 +11,19 @@ Public Class WalletConnection
     Private nodeurl As String
     Private nodeport As Integer
 
-    Public Class Assetinfo
-        Public Property assetID As String
-        Public AssetList As List(Of Assetinfo)
-        
+    Public Class Assetbalances
+    Public Property assetId as string
+    Public Property balance as double
+    Public property reissuable as boolean
+    Public property quantity as double
+    Public property issuetransaction as Transactioninfo
     End Class
+
+    Public Class Assetinfo
+        Public Property address As String
+        Public Property balances As List(Of Assetbalances)
+    End Class
+
     Public Class Transactioninfo
         Public Property type As Integer
         Public Property id As String
@@ -31,6 +40,7 @@ Public Class WalletConnection
         Public Property name As String
         Public Property description As String
     End Class
+
     Function Getassetlist()
         Dim json As String
         Dim wc As New WebClient
@@ -41,6 +51,7 @@ Public Class WalletConnection
         Dim getassetinfo As List(Of assetinfo) = getinfowallet.balances
         Return getassetinfo
     End Function
+
     Function Getassetname(AssetID As String)
         Dim json As String
         Dim wc As New WebClient
@@ -48,9 +59,13 @@ Public Class WalletConnection
         json = wc.DownloadString(rpcpath)
         Dim jss As New JavaScriptSerializer()
         Dim getinfowallet As Transactioninfo = jss.Deserialize(Of Transactioninfo)(json)
-        Dim getassetnamer As String = getinfowallet.name
+        Dim getassetnamer as string
+	For each getinfowallet.balances.issuetransaction in getinfowallet.balances
+	getassetnamer=getassetnamer & " " & getinfowallet.balances.issuetransaction.name
+	Next
         Return getassetnamer
     End Function
+
     Function Gettransactioninfo(TransactionID As String)
         Dim json As String
         Dim wc As New WebClient
@@ -60,6 +75,7 @@ Public Class WalletConnection
         Dim gettransaction As Transactioninfo = jss.Deserialize(Of Transactioninfo)(json)
         Return gettransaction
     End Function
+
     Function GetWavesBalance() As Double
         Dim json As String
         Dim wc As New WebClient
@@ -69,6 +85,7 @@ Public Class WalletConnection
         Dim getinfowallet As WalletConnection = jss.Deserialize(Of WalletConnection)(json)
         Return getinfowallet.balance / 100000000
     End Function
+
     Function GetAssetBalance(assetID As String, decimals As Integer) As Double
         Dim json As String
         Dim wc As New WebClient
@@ -78,13 +95,16 @@ Public Class WalletConnection
         Dim getinfowallet As WalletConnection = jss.Deserialize(Of WalletConnection)(json)
         Return getinfowallet.balance / (10 ^ decimals)
     End Function
+
     Public Sub New(ByVal parwalletaddress As String, ByVal parnodeurl As String, ByVal parnodeport As Integer)
         walletaddress = parwalletaddress
         nodeurl = parnodeurl
         nodeport = parnodeport
-
-
     End Sub
+
     Public Sub New()
+	walletaddress = "3PE9n5HRUsU6kjknatxPfvam7WmKy8EJcRW"
+	nodeurl = "localhost"
+	nodeport = "6869"
     End Sub
 End Class
